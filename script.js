95f42a410b0686056ab32f6a52321334a30a690a
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-11 17:47:51 -0700

    aesthetic css changes and js button menu toggle function changes

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 function hamburger() {
     var button = document.getElementById("container");
     var content = document.getElementById("container-content");
-  
-    if (button.value === "on") {
-      content.style.display = "flex";
-      button.value = "off";
+    var whole = document.getElementById("whole");
+    button.classList.toggle("change")
+    if(content.style.display !== 'flex') {
+      content.style.display = 'flex';
+      whole.style.opacity = 0.1;
     }
     else {
-      content.style.display = "none";
-      button.value = "on";
+      content.style.display = 'none';
+      whole.style.opacity = 1;
     }
-    }
+  }
 //end hamburger
 
 
