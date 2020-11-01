0f480ecff97c9b2a9f21bb25df7e333f689b8026
Julian Ranieri <jumpingafterrain@gmail.com>
2020-11-01 09:53:47 -0800

    landing

--- script.js
+++ script.js
@@ -15,36 +15,18 @@ function hamburger() {
   }
 //end hamburger
 
+//scroll function
+window.onscroll = () => {
+  scrollFunction()
+}
+function scrollFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var header = document.getElementById('header')
 
-//slideshow
-    var slideIndex = 1;
-    showSlides(slideIndex);
-    
-    // Next/previous controls
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-    
-    // Thumbnail image controls
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("demo");
-      var captionText = document.getElementById("caption");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-      captionText.innerHTML = dots[slideIndex-1].alt;
-    }
-//slidshow end
+  if(winScroll > 100) {
+    header.style.display = 'flex'
+ } else {
+   header.style.display = 'none'
+ }
+
+}
