c57ee79183d0ad4187df56c174704a534edd60fc
Julian ranieri <jumpingafterrain@gmail.com>
2022-01-27 21:36:59 -0800

    updated website

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
 // hamburger
 function hamburger() {
-    var button = document.getElementById("container");
-    var content = document.getElementById("container-content");
-    var whole = document.getElementById("whole");
-    button.classList.toggle("change")
-    if (content.style.display !== 'flex') {
-        content.style.display = 'flex';
-        whole.style.opacity = 0.1;
-    } else {
-        content.style.display = 'none';
-        whole.style.opacity = 1;
-    }
-}
-// end hamburger
-
-// scroll function
-window.onscroll = () => {
-    scrollFunction()
-}
-function scrollFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var header = document.getElementById('header')
-
-    if (winScroll > 10) {
-        header.style.display = 'flex'
-    } else {
-        header.style.display = 'none'
-    }
-
-    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-        document.getElementById("first-section").className = "slideUp";
-    }
-    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
-        document.getElementById("second-section").className = "slideUp2";
-    }
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("projects-div").className = "slideUp3";
-    }
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("projects-div2").className = "slideUp3";
-    }
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("projects-div3").className = "slideUp3";
-    }
-}
-
-// scroll from landing to main body
-function scrollFromLanding() {
-    var businessCard = document.getElementById("business-card")
-    businessCard.scrollIntoView({ behavior: 'smooth' })
+  var button = document.getElementById("ham-container");
+  var content = document.getElementById("ham-container-content");
+  var whole = document.getElementById("whole");
+  button.classList.toggle("ham-change")
+  if (content.style.display !== 'flex') {
+      content.style.display = 'flex';
+      whole.style.opacity = 0.1;
+  } else {
+      content.style.display = 'none';
+      whole.style.opacity = 1;
   }
+}
