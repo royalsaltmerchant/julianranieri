8a38ac2dc861137df41c0d724a0b88e853c39502
Julian ranieri <jumpingafterrain@gmail.com>
2022-02-15 15:35:51 -0800

    cleanup resizing etc.

--- script.js
+++ script.js
@@ -12,39 +12,3 @@ function hamburger() {
       whole.style.opacity = 1;
   }
 }
-
-// header
-function Header(props) {
-  this.props = props
-
-  this.render = function() {
-    this.elem = document.getElementById(this.props.id)
-    
-
-    var html = /*html*/ `
-      <a class="email" href="mailto:jumpingafterrain@gmail.com">Jumpingafterrain@gmail.com</a>
-      <div class="ham-container" id="ham-container" onclick="hamburger()">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-        <div id="ham-container-content">
-          <a class="menu-item" href="index.html">About</a>
-          <hr>
-          <a class="menu-item" href="#projects">Projects</a>
-          <hr>
-          <a class="menu-item" href="https://github.com/royalsaltmerchant">Github</a>
-          <hr>
-          <a class="menu-item" href="./JulianRanieriResumeWeb.pdf">Resume</a>
-        </div>
-      </div>
-    `
-    this.elem.innerHTML = html
-  }
-
-  this.render()
-  return this
-}
-
-var header = new Header({
-  id: 'header'
-})
